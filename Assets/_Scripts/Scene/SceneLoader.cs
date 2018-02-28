663d9986dd141922f257fa5e4c13e8de89800222
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-03-01 00:25:52 +0700

    Add Main Menu

---
+++ Assets/_Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Play(string sceneToLoad) {
+		SceneManager.LoadScene(sceneToLoad);
+	}
+	public void ExitGame() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+			Application.Quit();
+	}
+}
