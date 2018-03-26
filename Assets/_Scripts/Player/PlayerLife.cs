1c5e02c03723d720a718dfbf3952eb1f5e31fc32
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-03-27 06:25:08 +0700

    refactor life control out of playerLife

--- Assets/_Scripts/Player/PlayerLife.cs
+++ Assets/_Scripts/Player/PlayerLife.cs
@@ -3,37 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerLife : MonoBehaviour {
-	public Text textLife;
-	private int currentLife;
-	public int maxLife;
-	public GameObject gameOverPanel;
-	// Use this for initialization
-	void Start () {
-		Revive();
-		gameOverPanel.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	public void Revive() {
-		currentLife = maxLife;
-		textLife.text = currentLife.ToString();
-		gameOverPanel.SetActive(false);
-	}
-	public void GetDamage() {
-		currentLife = currentLife - 1;
-		textLife.text = currentLife.ToString();
-		if (currentLife <= 0) {
-			GameOver();
-		}
-	}
-	public void GameOver() {
-		gameOverPanel.SetActive(true);
-	}
-	public bool IsAlive() {
-		return (currentLife > 0);
-	}
+public class PlayerLife : MonoBehaviour
+{
+    public delegate void LifeEvent(Player player, int currentLife);
+    public event LifeEvent OnLifeChange;
+    public int currentLife { get; private set; }
+    public int maxLife;
+    private Player player;
+
+    void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    public void GetDamage()
+    {
+        currentLife = currentLife - 1;
+        OnLifeChange(player,currentLife);
+    }
+
+    public void SetLifeToMax()
+    {
+        currentLife = maxLife;
+    }
+
+    public bool IsAlive()
+    {
+        return (currentLife > 0);
+    }
 }
--- Assets/_Scripts/ShieldLevelManagerSTUB.cs
+++ Assets/_Scripts/ShieldLevelManagerSTUB.cs
@@ -1,11 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShieldLevelManagerSTUB : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+    public Text textLife;
+    private PlayerLife playerLife;
+
+    void Awake()
+    {
+        playerLife = FindObjectOfType<PlayerLife>();
+        playerLife.OnLifeChange += ResolveLifeChange;
+    }
+
+    void OnApplicationQuit()
+    {
+        playerLife.OnLifeChange -= ResolveLifeChange;    
+    }
+
+    private void ResolveLifeChange(Player player, int currentLife)
+    {
+        textLife.text = playerLife.currentLife.ToString();
+        if (currentLife <= 0) {
+            GameOver();
+        }
+    }
+
     void Start()
     {
         GameManager.Instance.ReadyTheBall();
+        RevivePlayer();
+    }
+
+    public void RevivePlayer() {
+		playerLife.SetLifeToMax();
+		textLife.text = playerLife.currentLife.ToString();
+		gameOverPanel.SetActive(false);
+	}
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
