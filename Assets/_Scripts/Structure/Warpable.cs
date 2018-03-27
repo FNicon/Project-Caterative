a1e582c432bac3f59a753bbb90f9995d8f21b1ac
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-03-28 00:46:41 +0700

    Delete ICollidable, Better warping system, Fix Camera values in scene

---
+++ Assets/_Scripts/Structure/Warpable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Caterative.Brick.Warp
+{
+    public class Warpable : MonoBehaviour
+    {
+        public enum State
+        {
+            Incoming,
+            WarpingOut,
+            WarpingIn,
+            Outgoing
+        }
+        public State state { get; private set; }
+
+        void Start()
+        {
+            state = State.Incoming;
+        }
+
+        public void Warp()
+        {
+            switch (state)
+            {
+                case State.Incoming:
+                    state = State.WarpingIn;
+                    break;
+                case State.WarpingIn:
+                    state = State.WarpingOut;
+                    break;
+                case State.WarpingOut:
+                    state = State.Outgoing;
+                    break;
+                case State.Outgoing:
+                    state = State.Incoming;
+                    break;
+                default:
+                    state = State.Incoming;
+                    break;
+            }
+        }
+    }
+}
