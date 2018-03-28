08e172686d93a89ffee89dbc15207ef723032f0c
kukuhbr <13515025@std.stei.itb.ac.id>
2018-03-28 17:04:00 +0700

    paddle moveable

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -2,54 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-	private Rigidbody2D playerBody;
-	private Transform cameraTransform;
-	public float playerSpeed;
-	public PlayerController playerController;
-	//public PlayerControllerAndroid playerControllerAndroid;
-	public PlayerAim playerAim;
-	public PlayerLife playerLife;
-	public GameObject camera;
-	public float playerMaxY;
-	public float playerMinY;
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    private Transform cameraTransform;
+    public float playerSpeed;
+    public PlayerController playerController;
+    public PlayerControllerAndroid playerControllerAndroid;
+    public PlayerAim playerAim;
+    public PlayerLife playerLife;
+    public float playerMaxY;
+    public float playerMinY;
 
-	// Use this for initialization
-	void Awake() {
-		playerBody = gameObject.GetComponent<Rigidbody2D>();
-		cameraTransform = camera.GetComponent<Transform> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		#if UNITY_EDITOR
+    // Use this for initialization
+    void Awake()
+    {
+        playerBody = gameObject.GetComponent<Rigidbody2D>();
+        cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        if (playerLife.IsAlive())
+        {
+            Move(playerController.ReadInput());
+            if (playerController.IsInputFire())
+            {
+                playerAim.Shoot();
+            }
+            ConstraintPlayerMovement();
+        }
+
+#elif UNITY_ANDROID
 			if (playerLife.IsAlive()) {
-			Move(playerController.ReadInput());
-	            if (playerController.IsInputFire()) {
-					playerAim.Shoot();
-				}
+				MoveAndroid(playerControllerAndroid.ReadInput());
+			}
 			ConstraintPlayerMovement();
-	        }
-		#elif UNITY_ANDROID
-		if (playerLife.IsAlive()) {
-			Move(playerControllerAndroid.ReadInput());
-		}
-		#endif
+#endif
     }
 
-	void Move(Vector2 input) {
-		Vector2 deltaVelocity = new Vector2 (input.x * playerSpeed, input.y * playerSpeed);
-		playerBody.velocity = deltaVelocity;
+    void Move(Vector2 input)
+    {
+        Vector2 deltaVelocity = new Vector2(input.x * playerSpeed, input.y * playerSpeed);
+        playerBody.velocity = deltaVelocity;
     }
 
-	void ConstraintPlayerMovement() {
-		float MaxY = playerMaxY + cameraTransform.position.y;
-		float MinY = playerMinY + cameraTransform.position.y;
-		Debug.Log ("Player pos = " + transform.position.y);
-		Debug.Log ("Max Y = " + MaxY);
-		Debug.Log ("Min Y = " + MinY);
-		Debug.Log ("Camera pos " + cameraTransform.position.y);
-		transform.position = new Vector2 (transform.position.x, Mathf.Clamp (transform.position.y, MinY, MaxY));
-	}
+    void MoveAndroid(Vector2 input)
+    {
+        playerBody.MovePosition(input);
+        Debug.Log("Rigidbody Pos" + playerBody.position);
+    }
+
+    void ConstraintPlayerMovement()
+    {
+        float MaxY = playerMaxY + cameraTransform.position.y;
+        float MinY = playerMinY + cameraTransform.position.y;
+        Debug.Log("Player pos = " + transform.position.y);
+        Debug.Log("Max Y = " + MaxY);
+        Debug.Log("Min Y = " + MinY);
+        Debug.Log("Camera pos " + cameraTransform.position.y);
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, MinY, MaxY));
+    }
 }
 
