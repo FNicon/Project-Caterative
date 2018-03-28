1d5b89d360658489cd980832a136651ba75d0187
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-03-28 12:39:05 +0700

    Add dummy cat and dummy trap

---
+++ Assets/_Scripts/Traps/DummyTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyTrap : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.collider.CompareTag("Player"))
+		{
+			Debug.Log("Trap Triggered! -1");
+		}
+	}
+}
