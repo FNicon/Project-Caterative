75439d9517afafb2540f5487dd978bd95603d110
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-05 12:19:28 +0700

    Add Brick and Wall SFX

---
+++ Assets/_Scripts/Wall/WallSFX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSFX : MonoBehaviour {
+	public AudioSource collisionAudio;
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.collider.CompareTag("Ball"))
+		{
+			collisionAudio.Play();
+		}
+	}
+}
