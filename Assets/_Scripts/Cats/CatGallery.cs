4f8bb76e4c05c3673dc2eb0040d311d9ca69eb3b
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-05-07 12:38:54 +0700

    Add gallery

---
+++ Assets/_Scripts/Cats/CatGallery.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatGallery : MonoBehaviour {
+	public CatType[] catsList;
+	public GameObject galleryObjectTemplate;
+	public List<GameObject> galleryObjects;
+	public Sprite unknownSpriteTemplate;
+	// Use this for initialization
+	void Start () {
+		CreateGallery();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateGallery() {
+		for (int i=0; i < catsList.Length; i++) {
+			galleryObjects.Add(Instantiate(galleryObjectTemplate,transform.position,transform.rotation));
+			galleryObjects[i].transform.SetParent(transform);
+			galleryObjects[i].GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
+			if (catsList[i].isAlreadySaved) {
+				galleryObjects[i].GetComponentsInChildren<Image>()[1].sprite = catsList[i].catSprite;
+				galleryObjects[i].GetComponentInChildren<Text>().text = catsList[i].catName;
+			} else {
+				galleryObjects[i].GetComponentsInChildren<Image>()[1].sprite = unknownSpriteTemplate;
+				galleryObjects[i].GetComponentInChildren<Text>().text = "unknown";
+			}
+		}
+	}
+}
