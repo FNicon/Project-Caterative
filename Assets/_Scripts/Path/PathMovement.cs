5d94c4d508892790f2131fe82220cc6ea16bc0d2
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-05-16 03:06:32 +0700

    minion movement and some fix

--- Assets/_Scripts/Path/PathMovement.cs
+++ Assets/_Scripts/Path/PathMovement.cs
@@ -1,37 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PathMovement : MonoBehaviour
 {
-	public Path pathToFollow;
-	public int currentSegment = 0;
-	//private bool isLastSegment = false;
-	public int moveToward = 1;
-	void Update ()
-	{
-		if (pathToFollow !=null)
-		{
-			Move();
-		}
-	}
-	void Move()
-	{
-		if (transform.position == pathToFollow.GetNodePosition(currentSegment))
-		{
-			currentSegment = currentSegment + moveToward;
-			if (currentSegment >= pathToFollow.pathNodes.Length - 1)
-			{
-				moveToward = -1;
-			} else if (currentSegment <= 0)
-			{
-				moveToward = 1;
+    public Path pathToFollow;
+    public int currentSegment = 0;
+    public int moveToward = 1;
+    public float speed = 1f;
+
+    public enum MovementType
+    {
+        Default,
+        Cycle
+    }
+
+    public MovementType movementType;
+
+    void Update()
+    {
+        if (pathToFollow != null)
+        {
+            switch (movementType)
+            {
+                case MovementType.Cycle:
+                    MoveCycle();
+                    break;
+                default:
+                    Move();
+                    break;
+            }
+        }
+        Vector3 newPosition = Vector3.MoveTowards(
+            transform.position, pathToFollow.GetNodePosition(currentSegment),
+            speed * Time.deltaTime
+        );
+        transform.position = newPosition;
+    }
+
+    private void MoveCycle()
+    {
+        if (transform.position == pathToFollow.GetNodePosition(currentSegment))
+        {
+            if (moveToward > 0)
+            {
+                currentSegment++;
+            } else {
+				currentSegment--;
 			}
-		}
-		Vector3 newPosition = Vector3.MoveTowards(
-			transform.position, pathToFollow.GetNodePosition(currentSegment),
-			1f * Time.deltaTime
-		);
-		transform.position = newPosition;
-	}
+            if (currentSegment >= pathToFollow.pathNodes.Length)
+            {
+                currentSegment = 0;
+            } else if (currentSegment < 0) {
+				currentSegment = pathToFollow.pathNodes.Length -1;
+			}
+        }
+    }
+
+    void Move()
+    {
+        if (transform.position == pathToFollow.GetNodePosition(currentSegment))
+        {
+            currentSegment = currentSegment + moveToward;
+            if (currentSegment >= pathToFollow.pathNodes.Length - 1)
+            {
+                moveToward = -1;
+            }
+            else if (currentSegment <= 0)
+            {
+                moveToward = 1;
+            }
+        }
+    }
 }
