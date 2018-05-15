e16addeadc68a21a0c79ff025389fdac6224b89c
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-05-16 02:01:18 +0700

    fixing unityeditor with if guard instead

--- Assets/_Scripts/Path/PathEditor.cs
+++ Assets/_Scripts/Path/PathEditor.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*using UnityEditor;
+using UnityEditor;
 
-[ExecuteInEditMode]*/
+[ExecuteInEditMode]
 public class PathEditor : MonoBehaviour
-{/*
-	public Path path;
-	private void OnDrawGizmos()
-	{
-		for (int i = 0; i<path.pathNodes.Length - 1;i++)
-		{
-			Handles.DrawDottedLine(path.pathNodes[i].position,path.pathNodes[i+1].position,3.0f);
-		}
-	}
-*/
+{
+    public Path path;
+    private void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = 0; i < path.pathNodes.Length - 1; i++)
+            {
+                Handles.DrawDottedLine(path.pathNodes[i].position, path.pathNodes[i + 1].position, 3.0f);
+            }
+        }
+    }
+
 }
+#endif
